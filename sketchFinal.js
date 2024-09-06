1d11061fe88630e58aa9b1834b18e4c04a9f7655
forgetfulcorner <89823915+forgetfulcorner@users.noreply.github.com>
2024-09-05 21:41:49 -0400

    Almost Final

--- sketchFinal.js
+++ sketchFinal.js
@@ -1,7 +1,7 @@
 let saveAllImages = true; // Toggle this variable to enable or disable saving all images
 let saveDelay = 200; // Delay in milliseconds between saves
-let draftMode = true;
-let currentIndex = 257; // Index to track the current entry being displayed
+let draftMode = false;
+let currentIndex = 0; // Index to track the current entry being displayed
 
 function saveAllEntries() {
 	if (saveAllImages) {
